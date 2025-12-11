998b602226b27ca83399933121d485030419205f
Matheus Peixoto <matheuspmenezes2000@gmail.com>
2025-12-11 01:52:18 -0300

    mudança logo

--- app/javascript/dashboard/components-next/sidebar/Sidebar.vue
+++ app/javascript/dashboard/components-next/sidebar/Sidebar.vue
@@ -618,8 +618,8 @@ const menuItems = computed(() => {
     </div>
     <section class="grid gap-2 mt-2 mb-4">
       <div class="flex gap-2 items-center px-2 min-w-0">
-        <div class="grid flex-shrink-0 place-content-center size-6">
-          <Logo class="size-4" />
+        <div class="grid flex-shrink-0 place-content-center">
+          <Logo class="w-4 h-auto" />
         </div>
         <div class="flex-shrink-0 w-px h-3 bg-n-strong" />
         <SidebarAccountSwitcher
