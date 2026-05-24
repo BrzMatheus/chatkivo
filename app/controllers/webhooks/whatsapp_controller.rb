8561e26476556479202a412de36e021e2a742fb4
Matheus Peixoto <matheuspmenezes2000@gmail.com>
2026-05-24 01:33:37 -0300

    fix(whatsapp): harden cloud webhook lookup

--- app/controllers/webhooks/whatsapp_controller.rb
+++ app/controllers/webhooks/whatsapp_controller.rb
@@ -29,13 +29,18 @@ class Webhooks::WhatsappController < ActionController::API
     ]
   end
 
+  def configured_meta_app_secrets
+    meta_app_secrets.compact_blank
+  end
+
   def whatsapp_channel
     @whatsapp_channel ||= whatsapp_business_payload_channel || Channel::Whatsapp.find_by(phone_number: params[:phone_number])
   end
 
   def meta_signature_verification_required?
-    return true if whatsapp_channel.blank?
+    return configured_meta_app_secrets.present? if whatsapp_channel.blank?
     return false unless whatsapp_channel.provider == 'whatsapp_cloud'
+    return false if configured_meta_app_secrets.blank?
     return true if channel_meta_app_secrets(whatsapp_channel).present?
 
     whatsapp_channel.provider_config['source'] == 'embedded_signup'
@@ -47,18 +52,43 @@ class Webhooks::WhatsappController < ActionController::API
     metadata = params.dig(:entry, 0, :changes, 0, :value, :metadata)
     return if metadata.blank?
 
-    phone_number = normalized_phone_number(metadata[:display_phone_number])
     phone_number_id = metadata[:phone_number_id]
+    channel = find_channel_by_phone_number_id(phone_number_id)
+    return channel if channel
+
+    phone_number = normalized_phone_number(metadata[:display_phone_number])
     channel = Channel::Whatsapp.find_by(phone_number: phone_number)
+    channel ||= find_channel_with_normalized_number(phone_number)
 
     return channel if channel && channel.provider_config['phone_number_id'] == phone_number_id
+  end
+
+  def find_channel_by_phone_number_id(phone_number_id)
+    return if phone_number_id.blank?
+
+    Channel::Whatsapp.find_by("provider_config ->> 'phone_number_id' = ?", phone_number_id)
+  end
+
+  def find_channel_with_normalized_number(phone_number)
+    normalized_phone_number = phone_number&.delete_prefix('+')
+    return if normalized_phone_number.blank?
+
+    normalizer = Whatsapp::PhoneNumberNormalizationService::NORMALIZERS.lazy.filter_map do |klass|
+      n = klass.new
+      n if n.handles_country?(normalized_phone_number)
+    end.first
+    return unless normalizer
+
+    Channel::Whatsapp.find_by(phone_number: "+#{normalizer.normalize(normalized_phone_number)}")
   end
 
   def normalized_phone_number(phone_number)
     return if phone_number.blank?
 
-    phone_number = phone_number.to_s
-    phone_number.start_with?('+') ? phone_number : "+#{phone_number}"
+    phone_number = phone_number.to_s.gsub(/[^0-9]/, '')
+    return if phone_number.blank?
+
+    "+#{phone_number}"
   end
 
   def inactive_whatsapp_number?
